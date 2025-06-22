81efeb135f4b9ec54f587afbae8c556d79b3344d
Sam McCagg <samuel.mccagg@gmail.com>
2025-06-22 00:28:43 -0400

    Clean up .

--- .rollup.cache/Users/smccagg/Desktop/code-projects/feathers/dist/index.js
+++
@@ -1,9 +0,0 @@
-// Styles
-import './lib/styles/globals.css';
-// Utils
-export { cn } from './lib/utils';
-// Components
-export { Button, buttonVariants } from './components/ui/button';
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent, } from './components/ui/card';
-export { Badge, badgeVariants } from './components/ui/badge';
-//# sourceMappingURL=index.js.map
