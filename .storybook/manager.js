4240a81bf957be7b41af0776b80989a8a809d131
Sam McCagg <samuel.mccagg@gmail.com>
2025-07-03 08:28:47 -0400

    Minor doc cleanup and customization.

---
+++ .storybook/manager.js
@@ -0,0 +1,18 @@
+import { addons } from 'storybook/manager-api';
+import { create } from 'storybook/theming/create';
+
+// Define shadcn/ui color palette
+const feathersTheme = create({
+  base: 'dark', // or 'dark' for dark theme
+
+  // Brand colors
+  brandTitle: '@futurehouse/feathers',
+  brandUrl: 'https://www.futurehouse.org/',
+  brandImage:
+    'https://cdn.prod.website-files.com/669f0d63ca36f46df2e0682f/66a0346885d0e329f9e79446_FutureHouse-Logo-White.svg', // Add your logo URL here if needed
+  brandTarget: '_self',
+});
+
+addons.setConfig({
+  theme: feathersTheme,
+});
